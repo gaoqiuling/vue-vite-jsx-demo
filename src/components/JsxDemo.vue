c42b11c25c0c429e49bcfe9557044039a6625eb7
gaoqiuling <gql@xinhuibaoli.com>
2024-05-07 11:14:12 +0800

    pinia存储

--- src/components/JsxDemo.vue
+++ src/components/JsxDemo.vue
@@ -1,12 +1,13 @@
 <script lang="jsx">
+import {useCountStore} from '@/stores/conter.ts'
 export default defineComponent({
   setup() {
+    const countStore=useCountStore();
     const count = ref(0);
-
     return () => (
           <el-button type="primary" 
           onclick={ () => { count.value++; }}> 
-          count is {count.value}
+          count is {count.value} ;store doublecount is:{countStore.doubleCount}
           </el-button>
     )
   },
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
 import { createApp } from 'vue'
-
+import { createPinia } from 'pinia'
 import Cookies from 'js-cookie';
 import './style.css'
 import ElementPlus from 'element-plus';
 import locale from 'element-plus/lib/locale/lang/zh-cn'; // 中文语言
 import App from './App.vue'
-
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 const app = createApp(App);
+app.use(createPinia());
 //挂载element-plus ICON
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
-
 // 使用element-plus 并且设置全局的大小
 app.use(ElementPlus, {
-    locale: locale,
-    size: Cookies.get('size') || 'medium'
-  });
-
+  locale: locale,
+  size: Cookies.get('size') || 'medium'
+});
 app.mount('#app')
